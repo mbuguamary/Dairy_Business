4681a8b5e2e00bdb63cbf6a4bfe4585a3369c0f0
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-07-16 17:00:53 +0300

    this commit adds the weekly production function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
+   
 
 function dailyProduction(){
-    var shedALitres=parseInt(document.getElementById("sheda").value);
-    var shedBLitres=parseInt(document.getElementById("shedb").value);
-    var shedCLitres=parseInt(document.getElementById("shedc").value);
-    var shedDLitres=parseInt(document.getElementById("shedd").value);
-   // document.getElementById("result").innerHTML = shedALitres+shedBLitres+shedCLitres+shedDLitres;
+var shedALitres=parseInt(document.getElementById("sheda").value);
+var shedBLitres=parseInt(document.getElementById("shedb").value);
+var shedCLitres=parseInt(document.getElementById("shedc").value);
+var shedDLitres=parseInt(document.getElementById("shedd").value);
+//
+    
     var sum=shedALitres+ shedBLitres+shedCLitres+ shedDLitres
+    
      dailyProduction;
    alert(""+sum);
+   document.getElementById("add").innerHTML = "Sum is="+sum;
 }
+  function WeeklyProduction(){
+var litres=parseInt(document.getElementById("litres").value);
+var sellingPrice=parseInt(document.getElementById("sprice").value);
+var days=parseInt(document.getElementById("days").value);
 
+    var total=litres*sellingPrice*days;
+    WeeklyProduction();
+    alert(""+total);
+    document.getElementById("add").innerHTML = "Sum is="+total;
+  }
 
 
 //console.log(shedALitres+shedBlitres+shedClitres+shedDlitres);
