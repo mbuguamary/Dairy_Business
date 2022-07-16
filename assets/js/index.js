822c136012c07f4a9ed7bfd0aa30ccebdbb47cb6
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-07-16 12:15:08 +0300

    this commit adds the first function

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+
+function dailyProduction(){
+    var shedALitres=document.getElementById("sheda").value;
+    var shedBLitres=document.getElementById("shedb").value;
+    var shedCLitres=document.getElementById("shedc").value;
+    var shedDLitres=document.getElementById("shedd").value;
+    document.getElementById("result").innerHTML = shedALitres+shedBLitres+shedCLitres+shedDLitres;
+    alert(shedALitres+shedBLitres);
+}
+//console.log(shedALitres+shedBlitres+shedClitres+shedDlitres);
+
+
+
+
+
+
+
+
+
+    
+    
+
