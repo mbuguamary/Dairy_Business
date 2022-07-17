4e77138b3dd693db9dd8acd70414ebffadd6075f
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-07-17 21:34:45 +0300

    this commit adds  leap year function

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,8 +31,83 @@ WeeklyProduction();
 
 
 
+function leapYearTotal(){
+    //total amount of milt in all sheds
+    let totalLitres = 1876;
+
+    let months = [
+        {
+            month:"January",
+            days: 31,
+            amount:  totalLitres(45 , 31) // jan
+        },
+        {
+            month:"February",
+            days: 29,
+            amount:  totalLitres(45 , 29) // feb
+        },
+        {
+            month:"March",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
+        {
+            month:"April",
+            days: 30,
+            amount:  totalLitres(45 , 30) // feb
+        },
+        {
+            month:"May",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
+        {
+            month:"June",
+            days: 30,
+            amount:  totalLitres(45 , 30) // feb
+        },
+        {
+            month:"July",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
+        {
+            month:"August",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
+        {
+            month:"September",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
+        {
+            month:"November",
+            days: 30,
+            amount:  totalLitres(45 , 30) // feb
+        },
+        {
+            month:"December",
+            days: 31,
+            amount:  totalLitres(45 , 31) // feb
+        },
 
 
+    ]
+
+    let rowsElement = document.getElementById("rows");
+    //print
+    months.map( i => {
+        rowsElement.append(`<tr>
+        <td>${i.month}</td>
+        <td>${total}</td>
+        <td>${i.days}</td>
+        <td>${i.amount}</td>
+    </tr>`)
+    })
+
+}
+leapYearTotal();
 
 
 
